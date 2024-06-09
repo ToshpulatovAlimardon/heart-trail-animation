c5be26726a6e4fdef8fc3ecef1e3a308964b4fec
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-06-09 12:36:58 +0500

    changed heart color

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ bodyEl.addEventListener("mousemove", (event) => {
   const xPos = event.offsetX;
   const yPos = event.offsetY;
   const spanEl = document.createElement("span");
+
   spanEl.style.left = xPos + "px";
   spanEl.style.top = yPos + "px";
-  bodyEl.appendChild(spanEl);
+
+  const size = Math.random() * 100;
+  spanEl.style.width = size + "px";
+  spanEl.style.height = size + "px";
+
+  bodyEl.append(spanEl);
+
+  setTimeout(() => {
+    spanEl.remove();
+  }, 3000);
 });
